5129fcd5b35433cfe40d58fc05c7580d6b59542e
TheWhiteHackers <nurefsanbera@gmail.com>
2024-01-10 12:23:01 -0600

    getting there....

---
+++ js.js
@@ -0,0 +1,14 @@
+let sumbox = document.getElementById("sumbox");
+let sizebox = document.getElementById("sizebox");
+
+function cal(){
+    if (sumbox.value === "" || sizebox.value === ""){
+        alert("please fill out all the number boxes");
+    } else {
+        console.log("yay");
+        
+
+
+        
+    }
+}
